38d9e78a253508d063f5bfc1e1c6dd44cc5de5c0
George <gpanterov@g-computer.(none)>
2014-10-13 17:21:14 -0400

    added spider for jobs.ru/moscow

--- spiders.py
+++ spiders.py
@@ -30,3 +30,26 @@ class JobsSpider(CrawlSpider):
 				pass
 		f.close()
 
+class JobsSpiderRu(CrawlSpider):
+	name="jobs_ru_spider"
+	allowed_domains = ['jobs.ru']
+	start_urls = ['http://www.jobs.ru/vacancy/region/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0/1/0']
+
+	rules = (
+	Rule(SgmlLinkExtractor(allow='/region/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0/1'), callback='parse_jobs',follow=True),
+	)
+
+	def parse_jobs(self, response):
+		hxs = HtmlXPathSelector(response)
+		# Scrape name and web addresses of company
+		titles = hxs.select("//div[@class='listrow']/a/text()").extract()
+		posting_text = hxs.select("//div[@class='listrow']/span[@class='listtext']/text()").extract()
+		posting_date = hxs.select("//div[@class='listrow']/span[@class='listdate']/text()").extract()
+		f = open('jobs_ru.csv', 'a')
+		for i in range(len(titles)):
+			f.write(titles[i].encode('utf8') + ';')
+			f.write(posting_text[i].encode('utf8') + ';')
+			f.write(posting_date[i].encode('utf8') + '\n')
+
+		f.close()
+
