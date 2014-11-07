9fc841f336390d1638c52228fbd9699c674da56d
George <gpanterov@g-computer.(none)>
2014-11-07 18:05:49 -0500

    ...

--- spiders.py
+++ spiders.py
@@ -3,10 +3,13 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.http import Request
-# asd
-# Test git
+import datetime
+
+
+
 class JobsSpider(CrawlSpider):
-	name="jobs_bg_spider"
+	country = "Bulgaria"
+	name = country + "_spider"
 	allowed_domains = ['jobs.bg']
 	start_urls = ['http://www.jobs.bg/front_job_search.php?first=1&str_regions=&str_locations=&tab=jobs&old_country=26&country=26&region=0&l_category%5B%5D=0&keyword=']
 
@@ -14,6 +17,13 @@ class JobsSpider(CrawlSpider):
 	Rule(SgmlLinkExtractor(allow='front_job_search\.php\?'), callback='parse_jobs',follow=True),
 	)
 
+	folder = 'scraped_data/' 
+	time_stamp = str(datetime.datetime.now())
+	fname = country + "_" + time_stamp
+	fpath = folder + fname
+	# Open file for appending and close it to erase any previous data
+	f = open(fpath, 'w')
+	f.close()
 	def parse_jobs(self, response):
 		hxs = HtmlXPathSelector(response)
 		# Scrape name and web addresses of company
@@ -21,18 +31,27 @@ class JobsSpider(CrawlSpider):
 		titles = hxs.select("//table//td[@width=280]//a[@class='MainLinkBold']/text()").extract()
 		companies = hxs.select("//table//td[@width=130]//a[@class='company_link']/text()").extract()
 		dates = dates[1:]
-		f = open('jobs.csv', 'a')
+		print titles
 		for i in range(len(dates)):
-			try:
-				f.write(dates[i].encode('utf8') + ',')
-				f.write(titles[i].encode('utf8') + ',')
-				f.write(companies[i].encode('utf8') + '\n')
-			except:
-				pass
+
+			self.write2file(dates[i], titles[i], companies[i])
+
+	def write2file(self, date, title, company):
+
+		f = open(self.fpath, 'a')
+		try:
+
+			f.write(date.encode('utf8') + ',')
+			f.write(title.encode('utf8') + ',')
+			f.write(company.encode('utf8') + '\n')
+		except:
+			pass
 		f.close()
 
+
 class JobsSpiderRu(CrawlSpider):
-	name="jobs_ru_spider"
+	country = "Russia"
+	name = country + "_spider"
 	allowed_domains = ['jobs.ru']
 	start_urls = ['http://www.jobs.ru/vacancy/region/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0/1/0']
 
@@ -40,17 +59,30 @@ class JobsSpiderRu(CrawlSpider):
 	Rule(SgmlLinkExtractor(allow='/region/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0/1'), callback='parse_jobs',follow=True),
 	)
 
+	folder = 'scraped_data/' 
+	time_stamp = str(datetime.datetime.now())
+	fname = country + "_" + time_stamp
+	fpath = folder + fname
+	# Open file for appending and close it to erase any previous data
+	f = open(fpath, 'w')
+	f.close()
+
 	def parse_jobs(self, response):
 		hxs = HtmlXPathSelector(response)
 		# Scrape name and web addresses of company
 		titles = hxs.select("//div[@class='listrow']/a/text()").extract()
 		posting_text = hxs.select("//div[@class='listrow']/span[@class='listtext']/text()").extract()
 		posting_date = hxs.select("//div[@class='listrow']/span[@class='listdate']/text()").extract()
-		f = open('jobs_ru.csv', 'a')
 		for i in range(len(titles)):
-			f.write(titles[i].encode('utf8') + ';')
-			f.write(posting_text[i].encode('utf8') + ';')
-			f.write(posting_date[i].encode('utf8') + '\n')
+			self.write2file(posting_date[i], titles[i], posting_text[i])
 
+	def write2file(self, date, title, company):
+
+		f = open(self.fpath, 'a')
+
+		f.write(date.encode('utf8') + ',')
+		f.write(title.encode('utf8') + ',')
+		f.write(company.encode('utf8') + '\n')
 		f.close()
+
 
