252e969da5f9c3347e42dad81a8df075d5a864f1
George <gpanterov@g-computer.(none)>
2014-11-07 19:59:19 -0500

    Russian spider crawls json and details

--- spiders.py
+++ spiders.py
@@ -3,9 +3,28 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.http import Request
+import scrapy.log
+
 import datetime
+import json
 
 
+class BaseJobsSpider(CrawlSpider):
+	def write2csv(self, date, title, company):
+
+		f = open(self.fpath, 'a')
+
+		f.write(date.encode('utf8') + ',')
+		f.write(title.encode('utf8') + ',')
+		f.write(company.encode('utf8') + '\n')
+		f.close()
+
+	def write2json(self, date, title, company):
+		f = open(self.fpath + '.json', 'a')
+		entry = {'date':date, 'title':title, 'company':company}
+		json.dump(entry, f)
+		f.write("\n")
+		f.close()
 
 class JobsSpider(CrawlSpider):
 	country = "Bulgaria"
@@ -31,7 +50,6 @@ class JobsSpider(CrawlSpider):
 		titles = hxs.select("//table//td[@width=280]//a[@class='MainLinkBold']/text()").extract()
 		companies = hxs.select("//table//td[@width=130]//a[@class='company_link']/text()").extract()
 		dates = dates[1:]
-		print titles
 		for i in range(len(dates)):
 
 			self.write2file(dates[i], titles[i], companies[i])
@@ -53,36 +71,48 @@ class JobsSpiderRu(CrawlSpider):
 	country = "Russia"
 	name = country + "_spider"
 	allowed_domains = ['jobs.ru']
-	start_urls = ['http://www.jobs.ru/vacancy/region/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0/1/0']
+#	start_urls = ['http://www.jobs.ru/vacancy/region/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0/1/0']
+	start_urls = ['http://www.jobs.ru']
 
 	rules = (
-	Rule(SgmlLinkExtractor(allow='/region/%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0/1'), callback='parse_jobs',follow=True),
-	)
+	Rule(SgmlLinkExtractor(allow='/vacancy/region/'), 
+				callback='parse_jobs',follow=True),
+			Rule(SgmlLinkExtractor(allow='/vacancy/view/'), 
+				callback='parse_details',follow=False),
+)
 
-	folder = 'scraped_data/' 
+	folder = 'scraped_data/Russia/' 
 	time_stamp = str(datetime.datetime.now())
 	fname = country + "_" + time_stamp
 	fpath = folder + fname
+	fpath_detail = folder + fname + '_detail.json'
 	# Open file for appending and close it to erase any previous data
-	f = open(fpath, 'w')
-	f.close()
+
+	logfile = open(fpath + ".log", 'w')
+	log_observer = scrapy.log.ScrapyFileLogObserver(logfile, level = scrapy.log.INFO)
+	log_observer.start()
 
 	def parse_jobs(self, response):
 		hxs = HtmlXPathSelector(response)
 		# Scrape name and web addresses of company
 		titles = hxs.select("//div[@class='listrow']/a/text()").extract()
+		urls = hxs.select("//div[@class='listrow']/a/@href").extract()
+
 		posting_text = hxs.select("//div[@class='listrow']/span[@class='listtext']/text()").extract()
 		posting_date = hxs.select("//div[@class='listrow']/span[@class='listdate']/text()").extract()
 		for i in range(len(titles)):
-			self.write2file(posting_date[i], titles[i], posting_text[i])
+			entry={'date':posting_date[i], 'title': titles[i],
+					'company' : posting_text[i], 'URL':'http://www.jobs.ru/' + urls[i]}
+			f = open(self.fpath + ".json", 'a')
+			json.dump(entry, f)
+			f.write("\n")
+			f.close()
 
-	def write2file(self, date, title, company):
-
-		f = open(self.fpath, 'a')
-
-		f.write(date.encode('utf8') + ',')
-		f.write(title.encode('utf8') + ',')
-		f.write(company.encode('utf8') + '\n')
+	def parse_details(self, response):
+		f = open(self.fpath_detail, 'a')
+		entry = {'URL':response.url, 'HTML':response.body}
+		json.dump(entry, f)
+		f.write("\n")
 		f.close()
 
 
